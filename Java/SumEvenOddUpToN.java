750c80bdd8dfa8e9e4396371df4f7d558d4a9516
Gautam Ahuja <goutamahuja8387@gmail.com>
2022-05-20 15:59:01 +0530

    Create SumEvenOddUpToN.java

---
+++ Java/SumEvenOddUpToN.java
@@ -0,0 +1,30 @@
+// Write a program to calculate and print the sums of even and odd integers of the first n natural numbers.
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ArrayIO
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scr = new Scanner(System.in);
+		int n = scr.nextInt();
+		int sum_even=0, sum_odd=0;
+		while(n>0)
+		{
+		    if(n%2==0)
+		    {
+		        sum_even = sum_even + n;
+		    }
+		    else
+		    {
+		        sum_odd = sum_odd + n;
+		    }
+		    n--;
+		}
+		System.out.println("Even: "+sum_even+"\nOdd: "+sum_odd);
+	}
+}
